64542998ae4519f912f1dd444f2fc01c2623a3ef
Brayan Angarita <brayanangarita11@gmail.com>
2021-07-30 18:58:58 -0500

    ''

--- class/base/connection.php
+++ class/base/connection.php
@@ -11,26 +11,27 @@ class connection {
 
    function __construct() {
       $dataList = $this->connection_data();
-      foreach ($dataList as $key => $value ){
-         $this->server = $value['server'];
-         $this->user = $value['user'];
-         $this->password = $value['password'];
-         $this->database = $value['database'];
-         $this->port = $value['port'];
-      }
+      // foreach ($dataList as $key => $value ){
+      //    $this->server = $value['server'];
+      //    $this->user = $value['user'];
+      //    $this->password = $value['password'];
+      //    $this->database = $value['database'];
+      //    $this->port = $value['port'];
+      // }
 
-      $this->db_connection = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
+      // $this->db_connection = new mysqli($this->server, $this->user, $this->password, $this->database, $this->port);
 
-      if($this->db_connection->connect_errno){
-         echo "Can not ready for database connection";
-         die();
-      }
+      // if($this->db_connection->connect_errno){
+      //    echo "We are not ready for database connection";
+      //    die();
+      // }
    }
 
    private function connection_data() {
       $url = dirname(__FILE__);
       $jsonData = file_get_contents($url . "/" . "config");
-      return json_decode($jsonData, true);
+      echo json_decode($jsonData, true);
+      return;
    }
      
 }
