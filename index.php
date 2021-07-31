0721d9ba6d33966c1a0b66a442ae39efe98f6413
Brayan Angarita <brayanangarita11@gmail.com>
2021-07-31 15:26:18 -0500

    ''

--- index.php
+++ index.php
@@ -16,93 +16,93 @@ header( 'Location: '.'auth.php' ) ;
 return;
 
 // No parameter means we require CONTEXT, USER, and LINK
-$LAUNCH = LTIX::requireData(); 
+// $LAUNCH = LTIX::requireData(); 
 
 
 
-if ( ! $USER->instructor ) {
-    //header( 'Location: '.U::addSession('student.php') ) ;
-    header( 'Location: '.U::addSession('auth.php') ) ;
-    return;
-}
+// if ( ! $USER->instructor ) {
+//     //header( 'Location: '.U::addSession('student.php') ) ;
+//     header( 'Location: '.U::addSession('auth.php') ) ;
+//     return;
+// }
 
-// If settings were updated
-if ( SettingsForm::handleSettingsPost() ) {
-    header( 'Location: '.U::addSession('index.php') ) ;
-    return;
-}
+// // If settings were updated
+// if ( SettingsForm::handleSettingsPost() ) {
+//     header( 'Location: '.U::addSession('index.php') ) ;
+//     return;
+// }
 
-// Model
-$p = $CFG->dbprefix;
-$old_code = Settings::linkGet('code', '');
+// // Model
+// $p = $CFG->dbprefix;
+// $old_code = Settings::linkGet('code', '');
 
-// View
-$menu = false;
-$menu = new \Tsugi\UI\MenuSet();
-$menu->addRight(__('Settings'), '#', /* push */ false, SettingsForm::attr());
+// // View
+// $menu = false;
+// $menu = new \Tsugi\UI\MenuSet();
+// $menu->addRight(__('Settings'), '#', /* push */ false, SettingsForm::attr());
 
-// Render view
-$OUTPUT->header();
-$OUTPUT->bodyStart();
-$OUTPUT->topNav($menu);
+// // Render view
+// $OUTPUT->header();
+// $OUTPUT->bodyStart();
+// $OUTPUT->topNav($menu);
 
-echo('<div style="float:right;">');
-echo('<form method="post" style="display: inline">');
-echo('<input type="submit" class="btn btn-warning" id="clear" name="clear" value="'.__('Clear data').'"><br/>'."\n");
-echo("</form>\n");
-echo('</div>');
+// echo('<div style="float:right;">');
+// echo('<form method="post" style="display: inline">');
+// echo('<input type="submit" class="btn btn-warning" id="clear" name="clear" value="'.__('Clear data').'"><br/>'."\n");
+// echo("</form>\n");
+// echo('</div>');
 
-$OUTPUT->welcomeUserCourse();
-echo('<br clear="all">');
-SettingsForm::start();
-echo("<p>Configure the LTI Tool<p>\n");
-echo("------------------- \n");
-SettingsForm::text('code',__('Code'));
-SettingsForm::checkbox('grade',__('Send a grade'));
-SettingsForm::text('match',__('Limit access by IP address.  This can be a prefix of an IP address like "142.16.41" or if it starts with a "/" it can be a regular expression (PHP syntax)'));
-echo("<p>Your current IP address is ".htmlentities(Net::getIP())."</p>\n");
-SettingsForm::done();
-// SettingsForm::end(false);
-SettingsForm::end(true); // Use API
+// $OUTPUT->welcomeUserCourse();
+// echo('<br clear="all">');
+// SettingsForm::start();
+// echo("<p>Configure the LTI Tool<p>\n");
+// echo("------------------- \n");
+// SettingsForm::text('code',__('Code'));
+// SettingsForm::checkbox('grade',__('Send a grade'));
+// SettingsForm::text('match',__('Limit access by IP address.  This can be a prefix of an IP address like "142.16.41" or if it starts with a "/" it can be a regular expression (PHP syntax)'));
+// echo("<p>Your current IP address is ".htmlentities(Net::getIP())."</p>\n");
+// SettingsForm::done();
+// // SettingsForm::end(false);
+// SettingsForm::end(true); // Use API
 
-?>
-<div id="alert"></div>
-<div id="attend-div"><img src="<?= $OUTPUT->getSpinnerUrl() ?>"></div>
-<?php
+// ?>
+// <div id="alert"></div>
+// <div id="attend-div"><img src="<?= $OUTPUT->getSpinnerUrl() ?>"></div>
+// <?php
 
-$OUTPUT->footerStart();
-$OUTPUT->templateInclude(array('attend'));
+// $OUTPUT->footerStart();
+// $OUTPUT->templateInclude(array('attend'));
 
-?>
-<script>
-$(document).ready(function(){
-    $.getJSON('<?= addSession('api/getrows.php') ?>', function(rows) {
-        window.console && console.log(rows);
-        context = { 'rows' : rows,
-            'instructor' : true,
-            'old_code' : '<?= $old_code ?>'
-        };
-        tsugiHandlebarsToDiv('attend-div', 'attend', context);
-    }).fail( function() { alert('getJSON fail'); } );
+// ?>
+// <script>
+// $(document).ready(function(){
+//     $.getJSON('<?= addSession('api/getrows.php') ?>', function(rows) {
+//         window.console && console.log(rows);
+//         context = { 'rows' : rows,
+//             'instructor' : true,
+//             'old_code' : '<?= $old_code ?>'
+//         };
+//         tsugiHandlebarsToDiv('attend-div', 'attend', context);
+//     }).fail( function() { alert('getJSON fail'); } );
 
-    $("#clear").click(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: '<?= U::addSession('api/clear.php') ?>',
-            dataType: 'json',
-            contentType: 'application/json',
-            data: JSON.stringify({}),
-            success: function (data) {
-                location.reload();
-            }
-        });
-    });
-    $(document).ready(function(){
-        tsugiHandlebarsToDiv('attend-div', 'attend', {});
-    });
-});
-</script>
+//     $("#clear").click(function(e) {
+//         e.preventDefault();
+//         $.ajax({
+//             type: "POST",
+//             url: '<?= U::addSession('api/clear.php') ?>',
+//             dataType: 'json',
+//             contentType: 'application/json',
+//             data: JSON.stringify({}),
+//             success: function (data) {
+//                 location.reload();
+//             }
+//         });
+//     });
+//     $(document).ready(function(){
+//         tsugiHandlebarsToDiv('attend-div', 'attend', {});
+//     });
+// });
+// </script>
 <?php
 $OUTPUT->footerEnd();
 
