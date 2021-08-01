267e145cd0ffc253b09fee807fa2dd78ec4dee81
Brayan Angarita <brayanangarita11@gmail.com>
2021-08-01 10:19:23 -0500

    ''

---
+++ api/getUsers.php
@@ -0,0 +1,15 @@
+<?php
+require_once "../config.php";
+require_once "class/base/connection.php";
+
+$connection = new connection;
+
+//$query = "SELECT * from lti_user";
+//$query = "INSERT into attend_api (link_id, user_id, attend, ipaddr, updated_at) VALUES (1, 3, '2021-07-30', '186.86.32.186', '2021-07-30 21:26:02')";
+//print_r($connection->nonQuery($query));
+$query = "SELECT * FROM attend";
+
+print_r($connection->getData($query));
+
+
+?>
